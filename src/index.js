8d637d283b847350307b27c448aae8b3d2608f30
Andrea Bertran <bertran.andrea@gmail.com>
2025-11-09 11:53:09 +0100

    UX improvements

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ function generateResponse(event) {
   let responseContainer = document.querySelector("#response-container");
 
   responseContainer.classList.add("response-container");
+  responseContainer.innerHTML = `<div class="blink"> Generating the response...</div>`;
 
   let userInput = document.querySelector("#user-input").value;
   let apiKey = "of26b55307144beab6f326a4tefcaa5e";
-  let prompt = `You are an expert observer of philosophy tasked with evaluating and critiquing ideas from historical philosophers. Briefly summarize ${userInput}'s core work and ideas. Then provide a humorous, darkly comedic evaluation of how their philosophy holds up today, focusing on relevance and applicability to contemporary societal, technological, political, and ethical issues. Keep the response under 1000 characters and break it down into two paragraphs, first their work and then the evaluation. Separate both by adding <br><br> between them. Make sure you challenge their views, highlight contradictions, and contextualize their ideas for modern times.`;
+  let prompt = `You are an expert observer of philosophy and fortune teller tasked with evaluating and critiquing ideas from historical philosophers. Briefly summarize ${userInput}'s core work and ideas. Then provide a humorous, darkly comedic evaluation of how their philosophy holds up today, focusing on relevance and applicability to contemporary societal, technological, political, and ethical issues. Keep the response under 1000 characters and break it down into two paragraphs, first their work and then the evaluation. Separate both by adding <br><br> between them. Make sure you challenge their views, highlight contradictions, and contextualize their ideas for modern times while keeping a fortune teller tone.`;
 
   let context =
     "You are an expert observer of contemporary society with deep understanding of today's cultures, global economy, and environmental challenges. You possess extensive knowledge of world history and the evolving systems of governance and power. You are well-versed in current societal concerns, including environmental sustainability, economic shifts, technological advancements, political ideologies, and social justice movements. Your expertise spans technology, ethics, and politics, enabling you to critically analyze and contextualize ideas within today's complex, multipolar world.";
